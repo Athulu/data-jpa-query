a89c39e481f479b9e8a53d1ddaeff4a0560ce5e8
Nikodem Wójciak <nikodem1703@gmail.com>
2022-08-18 15:40:30 +0200

    initial commit

---
+++ src/main/java/com/example/datajpaquery/DataJpaQueryApplication.java
@@ -0,0 +1,13 @@
+package com.example.datajpaquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaQueryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datajpaquery/DataJpaQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datajpaquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
